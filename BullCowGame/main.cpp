a78052c086152d64a865163e283b54847cd9b5fd
Björn <bjorn.alfsson@gmail.com>
2019-02-17 13:26:34 +0100

    getline in place

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -1,9 +1,19 @@
+#include <iostream>
+#include <string>
 
-#include "iostream"
+using namespace std;
 
+int main() {
+	
+	string Guess = "";
+	constexpr int WORD_LENGTH = 6;
 
-int main()
-{
-	std::cout << "Hello World";
+	cout << "Welcome to Bulls and Cows" << endl;
+	cout << "Guess the isogram that has " << WORD_LENGTH << " letters in it!" << endl;
+
+	cout << "Please, enter your guess: ";
+	getline(cin, Guess);
+	cout << "You guessed on the word " << Guess << endl;
+
 	return 0;
 }
