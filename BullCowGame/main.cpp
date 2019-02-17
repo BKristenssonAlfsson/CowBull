86291c8322303eaf3aaeb3cf7c3bc6efa30b3ba9
Björn <bjorn.alfsson@gmail.com>
2019-02-17 17:01:47 +0100

    Added menu

--- BullCowGame/main.cpp
+++ BullCowGame/main.cpp
@@ -5,30 +5,57 @@ using namespace std;
 
 string Guess = "";
 constexpr int WORD_LENGTH = 6;
+constexpr int amountOfGuesses = 5;
+
 
 void Intro();
+void PlayGame();
 string GetGuess();
+bool AskToPlayAgain();
 
 int main() {
 	
 	Intro();
-
-	GetGuess();
-
-	cout << "You guessed on the word " << Guess << endl;
-
+	do {
+		PlayGame();
+	} while (AskToPlayAgain() == true);
+	
 	return 0;
 }
 
 void Intro() {
 	cout << "Welcome to Bulls and Cows" << endl;
 	cout << "Guess the isogram that has " << WORD_LENGTH << " letters in it!" << endl;
-
+	cout << endl;
 	return;
+}
+
+void PlayGame() {
+	for (int i = 1; i <= amountOfGuesses; i++) {
+		cout << "Guess number: " << i << endl;
+		GetGuess();
+		cout << "You guessed on the word " << Guess << endl;
+		cout << endl;
+	}
 }
 
 string GetGuess() {
 	cout << "Please, enter your guess: ";
 	getline(cin, Guess);
 	return Guess;
+}
+
+bool AskToPlayAgain() {
+
+	cout << "Do you want to play again? ";
+	string Response = "";
+	getline(cin, Response);
+
+	if (Response[0] == 'y' || Response[0] == 'Y') {
+		return true;
+	}
+	else {
+		return false;
+	}
+
 }
